2faf09d9386f5f159ca01162c41d81e738c33b22
Benjamin Gulliver <fatherbenooo@gmail.com>
2019-07-05 17:34:45 +1000

    Added basic player movement

---
+++ Assets/Characters/Scripts/stats_global.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stats_global : MonoBehaviour
+{
+    public float movementSpeed = 0.0f;
+    public float attackDamage  = 0.0f;
+    public float attackSpeed   = 0.0f;
+    public float attackStamina = 0.0f;
+    public float dodgeDistance = 0.0f;
+    public float dodgeCooldown = 0.0f;
+    public float dodgeSpeed    = 0.0f;
+    public float dodgeStamina  = 0.0f;
+    public float healthPoints  = 0.0f;
+    public float staminaPoints = 0.0f;
+}
