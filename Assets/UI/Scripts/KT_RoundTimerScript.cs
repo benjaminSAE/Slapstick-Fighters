07655952e7af69bb6d7f1c1a88b7ce89af9b437b
Kevin T <52388381+kevinatSAE@users.noreply.github.com>
2019-07-08 12:02:41 +1000

    GameOver + CountDown
    done

---
+++ Assets/UI/Scripts/KT_RoundTimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KT_RoundTimerScript : MonoBehaviour
+{
+    float currentTime = 0f;
+    //time in seconds
+    [SerializeField] public float startingTime = 180f;
+    public Text CountdownText;
+  
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change 1 * deltatime if you want the game to countdown faster, 3 * for 3x faster etc
+        currentTime -= 1 * Time.deltaTime;
+        CountdownText.text = currentTime.ToString("0"); 
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+            SceneManager.LoadScene(1);
+        }
+    }
+}
