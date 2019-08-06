e2ea34acea273ce8c5a532eba2934f114b0ee5e2
KeananS <52388352+KeananS@users.noreply.github.com>
2019-08-06 10:12:46 +1000

    Main Menu V5
    Work in Progress

---
+++ Assets/Main/Scripts/AG_GlobalEnums.cs
@@ -0,0 +1,22 @@
+﻿
+/// <summary>
+/// This enum allows you to access the three character types from any script in your project.
+/// </summary>
+public enum CharacterType {
+    /// <summary>
+    /// We haven't set our character type yet.
+    /// </summary>
+    Undefined = 0,
+    /// <summary>
+    /// The archer.
+    /// </summary>
+    Archer = 1,
+    /// <summary>
+    ///  The knight.
+    /// </summary>
+    Knight = 2,
+    /// <summary>
+    /// The tank.
+    /// </summary>
+    Tank = 3
+};
