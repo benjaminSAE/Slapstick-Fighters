9a5a29be18c37e74cd3e3816c63f180845e12662
kevinatSAE <52388381+kevinatSAE@users.noreply.github.com>
2019-08-12 00:56:04 +1000

    Made progress on animations
    Made blend tree, scripts, new scene and animators

---
+++ Assets/Characters/Scripts/KT_AnimatorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KT_AnimatorScript : MonoBehaviour
+{
+    public Animator animator;
+    public float InputX;
+    public float InputY;
+
+    void Start()
+    {
+        //start the animator
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        animator.SetFloat("InputX", InputX);
+        InputY = Input.GetAxis("Vertical");
+        animator.SetFloat("InputY", InputY);
+    }
+}
