61aabb38b2f6618da0f3d57f9df1a1c8e33d5526
Kevin T <52388381+kevinatSAE@users.noreply.github.com>
2019-08-13 12:07:41 +1000

    Finished respawn animation

---
+++ Assets/Characters/Scripts/KT_RespawnAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KT_RespawnAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private KeyCode up;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up))
+        {
+            animator.SetBool("isRespawning", true);
+        }
+        else
+        {
+            animator.SetBool("isRespawning", false);
+        }
+    }
+}
+
