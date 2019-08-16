b5cef33a6a71b8349e35c78ca8ef95780d0bfe59
kevinatSAE <52388381+kevinatSAE@users.noreply.github.com>
2019-08-16 20:43:23 +1000

    Tank attack good

--- Assets/Characters/Scripts/attack_tank.cs
+++ Assets/Characters/Scripts/attack_tank.cs
@@ -38,7 +38,7 @@ public class attack_tank : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {      
+    {
         //updating "rotation" to equal the same value inside "global_movement_controls"
         rotation = gameObject.GetComponent<global_movement_controls>().rotation;
 
@@ -48,12 +48,7 @@ public class attack_tank : MonoBehaviour
         //running custom method stated below
         TankAttack();
 
-        if(whileLoop == false)
-        {
-            animator.SetBool("isTankAttacking", false);
-        }
 
-        whileLoop = false;
 
     }
 
@@ -62,155 +57,46 @@ public class attack_tank : MonoBehaviour
     {
         if (currentStaminaPoints > attackStamina && Time.time > nextAttack && Input.GetKeyDown(attack))
         {
-            //upwards attack movement
-            if (rotation > -3 && rotation < 3)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 top = new Vector3(0, 0, attackDistance * attackDistanceBase);
-                StartCoroutine(smooth_move(top, 1f));
+            animator.SetBool("isTankAttacking", true);
+            //start clockwise rotation of sword with a Coroutine;
+            //StartCoroutine(RotateMe(Vector3.up * 90, 0.8f));
 
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
+            //setting time before second swing
 
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
 
-            //upwards/right attack movement
-            if (rotation > 42 && rotation < 48)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 topRight = new Vector3(attackDistance * attackDistanceBase * attackDistanceBaseCorner, 0, attackDistance * attackDistanceBase * attackDistanceBaseCorner);
-                StartCoroutine(smooth_move(topRight, 1f));
+            //setting the cooldown before the next attack can be activated
+            nextAttack = Time.time + attackCooldown;
 
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
+            //runs the method from "global_stamina" to use an amount of stamina points
+            global_stamina stamina_globalInstance = GetComponent<global_stamina>();
+            stamina_globalInstance.AttackStamina();
 
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
-
-            //right attack movement
-            if (rotation > 87 && rotation < 93)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 right = new Vector3(attackDistance * attackDistanceBase, 0, 0);
-                StartCoroutine(smooth_move(right, 1f));
-
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
-
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
-
-            //downwards/right attack movement
-            if (rotation > 132 && rotation < 138)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 bottomRight = new Vector3(attackDistance * attackDistanceBase * attackDistanceBaseCorner, 0, -attackDistance * attackDistanceBase * attackDistanceBaseCorner);
-                StartCoroutine(smooth_move(bottomRight, 1f));
-
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
-
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
-
-            //downwards attack movement
-            if (rotation > 177 && rotation < 183 || rotation > -183 && rotation < -177)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 bottom = new Vector3(0, 0, -attackDistance * attackDistanceBase);
-                StartCoroutine(smooth_move(bottom, 1f));
-
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
-
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
-
-            //downwards/left attack movement
-            if (rotation > -138 && rotation < -132)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 bottomLeft = new Vector3(-attackDistance * attackDistanceBase * attackDistanceBaseCorner, 0, -attackDistance * attackDistanceBase * attackDistanceBaseCorner);
-                StartCoroutine(smooth_move(bottomLeft, 1f));
-
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
-
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
-
-            //left attack movement
-            if (rotation > -93 && rotation < -87)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 left = new Vector3(-attackDistance * attackDistanceBase, 0, 0);
-                StartCoroutine(smooth_move(left, 1f));
-
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
-
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
-
-            //upwards/left attack movement
-            if (rotation > -48 && rotation < -42)
-            {
-                animator.SetBool("isTankAttacking", true);
-                //setting the direction of the Coroutine and starting the Coroutine
-                Vector3 topLeft = new Vector3(-attackDistance * attackDistanceBase * attackDistanceBaseCorner, 0, attackDistance * attackDistanceBase * attackDistanceBaseCorner);
-                StartCoroutine(smooth_move(topLeft, 1f));
-
-                //setting the cooldown before the next attack can be activated
-                nextAttack = Time.time + attackCooldown;
-
-                //runs the method from "global_stamina" to use an amount of stamina points
-                global_stamina stamina_globalInstance = GetComponent<global_stamina>();
-                stamina_globalInstance.AttackStamina();
-            }
+            BG_CharacterAudio characterAudioInstance = GetComponent<BG_CharacterAudio>();
+            characterAudioInstance.PlayerSounds(BG_CharacterAudio.soundList.TankAttack);
         }
-
-        //BG_CharacterAudio characterAudioInstance = GetComponent<BG_CharacterAudio>();
-        //characterAudioInstance.PlayerSounds(BG_CharacterAudio.soundList.TankAttack);
-    }
-
-    //method that runs the Coroutine
-    IEnumerator smooth_move(Vector3 direction, float speed)
-    {
-        float startime = Time.time;
-        Vector3 start_pos = transform.position; //Starting position.
-        Vector3 end_pos = transform.position + direction; //Ending position.
-
-        while (start_pos != end_pos && ((Time.time - startime) * speed) < 1f)
+        else
         {
-            float move = Mathf.Lerp(0, 1, (Time.time - startime) * speed);
-
-            transform.position += direction * move;
-
-            whileLoop = true;
-
-            yield return null;
+            animator.SetBool("isTankAttacking", false);
         }
     }
 }
+
+//    //method that runs the Coroutine
+//    IEnumerator smooth_move(Vector3 direction, float speed)
+//    {
+//        float startime = Time.time;
+//        Vector3 start_pos = transform.position; //Starting position.
+//        Vector3 end_pos = transform.position + direction; //Ending position.
+
+//        while (start_pos != end_pos && ((Time.time - startime) * speed) < 1f)
+//        {
+//            float move = Mathf.Lerp(0, 1, (Time.time - startime) * speed);
+
+//            transform.position += direction * move;
+
+//            whileLoop = true;
+
+//            yield return null;
+//        }
+//    }
+//}
