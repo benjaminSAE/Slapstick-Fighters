ef0f1e556aa49345caa46698aeb7f9f1bc6ed7bd
Benjamin Gulliver <fatherbenooo@gmail.com>
2019-08-16 19:12:23 +1000

    Added menu music and button clicking sounds

---
+++ Assets/Audio/Scripts/BG_MusicAudio_MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_MusicAudio_MainMenu : MonoBehaviour
+{
+    FMOD.Studio.EventInstance mainMenuMusicMusic;
+    [FMODUnity.EventRef] [SerializeField] private string mainMenuMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenuMusicMusic = FMODUnity.RuntimeManager.CreateInstance(mainMenuMusic);
+        mainMenuMusicMusic.start();
+    }
+
+    public void EndMusic()
+    {
+        mainMenuMusicMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
---
+++ Assets/Audio/Scripts/BG_MusicAudio_MenuClicks.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_MusicAudio_MenuClicks : MonoBehaviour
+{
+    FMOD.Studio.EventInstance forwardClick;
+    FMOD.Studio.EventInstance backwardsClick;
+    FMOD.Studio.EventInstance selectClick;
+
+    [FMODUnity.EventRef] [SerializeField] private string forwardClickSound;
+    [FMODUnity.EventRef] [SerializeField] private string backwardsClickSound;
+    [FMODUnity.EventRef] [SerializeField] private string selectClickSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        forwardClick = FMODUnity.RuntimeManager.CreateInstance(forwardClickSound);
+        backwardsClick = FMODUnity.RuntimeManager.CreateInstance(backwardsClickSound);
+        selectClick = FMODUnity.RuntimeManager.CreateInstance(selectClickSound);
+    }
+
+    public void ForwardsClick()
+    {
+        forwardClick.start();
+    }
+
+    public void BackwardsClick()
+    {
+        backwardsClick.start();
+    }
+
+    public void SelectClick()
+    {
+        selectClick.start();
+    }
+}
